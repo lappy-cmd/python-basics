b5c18630e3666b776d6d958a98b302360bbddd32
= <=>
2026-02-16 20:09:34 -0800

    data types and operators

---
+++ datatypesnoperators.py
@@ -0,0 +1,77 @@
+# #4DATATYPES:
+# # 1. string, 2. integer, 3. float, 4. boolean
+
+# #   string- anything in "" (quotation marks) ex: "hello", "hi", "I have 1 dog in my house."
+# print ("hello")
+
+
+# #   integer - any number (5, 22, 9, etc)
+# print (5) 
+
+
+# #   float - decimal numbers/fractions (3.2, 5.2, 8.0)
+# print (4.5)
+
+
+# #   boolean - only 2 - True/False (capitals matter)
+# print (True)
+
+# x="5"
+# #in order to convert data types, you need a casting:
+
+# print (int(x))
+
+# y = "5"
+# print (x+y)
+# #(prints 55)
+# #can't make a non-number an integer
+# print (int(x) + int(y))
+
+# z=5
+# print (float(z))
+# a=5.6
+# print (int(a))
+
+
+
+x=7
+y=3
+
+print (x+y)
+print (22+22)
+print (5-2)
+print (5*2)
+print (5/2)
+
+
+print (5//2)
+#floor division - printed 2(decimal is removed)
+
+
+print (5%25)
+#gives the remainder - modulo
+
+#exponents - not ^, but **
+print (5**2)
+
+#>, <, ==, <=, >=, != (not equal to)
+
+print (5>2)
+print (5<2)
+
+print (5<=2)
+print (5>=2)
+
+print (5!=2)
+
+#git steps
+
+#1. git init
+#2. git config --global user.name "ashvik"
+#3. git config --global user.email "ashvik.sekar@gmail.com"
+#4. git add. .
+#5. git commit -m "message"
+# next, create the repo(repository) on github
+#6. git branch -M main
+#7. git remode add origin (link)
+#8. git push origin main
