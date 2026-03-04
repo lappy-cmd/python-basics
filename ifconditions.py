fd80ca85cf5f88b2706e2b8ef1c50fe8c6955f26
= <=>
2026-03-03 19:35:47 -0800

    if conditions/inputs

---
+++ ifconditions.py
@@ -0,0 +1,84 @@
+# If/Else input learning
+# 
+# 
+# 
+# sky = input("What color is the sky?: ")
+
+# if (sky=="blue"):
+#     print("correct")
+# else:
+#     print("wrong")
+
+# userage = int(input("What is your age?: "))
+
+# if userage >= 16:
+#     print ("You can drive.")
+# elif userage < 0:
+#     print ("Invalid input")
+# else:
+#     print ("You need to wait ",16 - userage, " years until you can drive.")
+
+# number = int(input("Pick any number.: "))
+# if number%2 == 0:
+#     print("Even")
+# # else:3
+# #     print("Odd")
+
+# grade = int(input("What grade are you in?: "))
+# age = int(input("What age are you?: "))
+
+
+# if age < 8 or grade < 3:
+#     print("U cant play the game")
+# else:
+# #     print ("You can play the game.")
+    
+# cost = float(input("What is your purchase price?: "))
+
+# # if cost <= 10:
+# #     print ("You qualify for a 10% discount. Your price is $", round(cost*0.9, 2))
+# # else:
+# #     print ("You qualify for a 20% discount. Your price is $", round(0.8*cost, 2))
+    
+# grade = float(input("What grade did you get on the test?: "))
+
+# if grade >= 90:
+#     print ("You got an A")
+# elif grade >= 80:
+#     print ("You got a B")
+# elif grade >= 70:
+#     print ("You got a C")
+# elif grade >= 60:
+#     print ("You got a D")
+# elif grade < 0:
+#     print("Invalid input.")
+# else:
+# #     print ("You got an F")
+
+
+# vacay = input("Do you have an upcoming vacation? (Answer Yes or No.): ")
+# if vacay == "Yes":
+#     grade = int(input("What grade are you in?: "))
+#     if grade == 12:
+#         print ("Great time to study for SAT.")
+#     elif grade > 12 or grade < 1:
+#         print ("Invalid input.")
+#     else:
+#         print ("Have an adventurous vacation.")
+# elif vacay == "No":
+# #     print ("Lock in at school.")
+# # else:
+# #     print ("Invalid input.")
+
+# year = int(input("What year is it?: "))
+
+# if year%4 != 0:
+#     print ("Not a leap year.")
+# else:
+#     if year%100 != 100:
+#         print ("Leap year")
+#     else:
+#         if year%400 == 0:
+#             print ("Leap year")
+#         else:
+#             print ("Not a leap year")
